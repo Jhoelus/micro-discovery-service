4e1ade894f8de1e6d19f5f7fbae5ea359a789b32
Jhoelus <joelhernandzn@gmail.com>
2025-01-14 13:34:42 -0600

    Config to Discovery Service

---
+++ src/test/java/com/xideral/micro_discovery_service/MicroDiscoveryServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.xideral.micro_discovery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+class MicroDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
